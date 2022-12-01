90f8682136f9133102cac19bf399673d120eb472
Constantin <grishanoff.konstantin@yandex.ru>
2022-12-01 13:53:23 +0500

    compl 1,2

---
+++ Desktop/java/seminar2/Main.java
@@ -0,0 +1,31 @@
+package seminar2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Создать новый продукт на основе Product,
+// сделать ему хотя бы одно свойство
+// включить в список продуктов вендинг машины
+// переопределить toString для вывода нового поля
+
+public class Main {
+
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        System.out.println("Can you hear from zoo");
+        zoo.talk();
+        System.out.println("Runable");
+        zoo.run();
+        System.out.println("SwimAble");
+        zoo.swim();;
+        System.out.println("Flyable");
+        zoo.fly();
+
+       
+     
+
+
+        
+    }
+   
+}
